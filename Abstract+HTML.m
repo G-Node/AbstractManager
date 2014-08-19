f7efc6d3c11c265817b21d972610d56a6438c51a
Christian Kellner <christian@kellner.me>
2014-08-19 12:28:46 +0200

    Abstract+HTML use GCA-iOS formatting code

--- Abstract+HTML.m
+++ Abstract+HTML.m
@@ -9,6 +9,7 @@
 #import "Abstract+HTML.h"
 #import "Author.h"
 #import "Author+Format.h"
+#import "Organization+Format.h"
 #import "Organization.h"
 #import "Affiliation.h"
 #import "Correspondence.h"
@@ -27,65 +28,94 @@
 - (NSString *)renderHTML
 {
     NSMutableString *html = [[NSMutableString alloc] init];
-    
-    [html appendString:@"<html><body>"];
-    [html appendFormat:@"<div><small>%@</small></div>", self.topic];
-    [html appendFormat:@"<div><h2>%@</h2></div>", self.title];
-    
-    [html appendString:@"<div><h3>"];
+
+    [html appendString:@"<html><head>"];
+    [html appendString:@"<link rel=\"stylesheet\" type=\"text/css\" href=\"Abstract.css\" />"];
+    [html appendString:@"<meta name='viewport' content='initial-scale=1.0,maximum-scale=10.0'/>"];
+    [html appendString:@"</head><body>"];
+    //[html appendFormat:@"<div id=\"aid\">%@</div>", [self formatId:YES]];
+    [html appendFormat:@"<div id=\"topic\">%@</div>", self.topic];
+    [html appendFormat:@"<div><h1 id=\"title\">%@</h2></div>", self.title];
+
+    [html appendString:@"<div><h2 id=\"author\">"];
     for (Author *author in self.authors) {
         [html appendFormat:@"%@", [author formatName]];
-        
-        [html appendFormat:@"<sup>"];
-        for (NSUInteger i = 0; i < self.affiliations.count; i++) {
+
+        [html appendFormat:@"<sup id=\"epithat\">"];
+        NSUInteger count = 0;
+        for (short i = 0; i < self.affiliations.count; i++) {
             Affiliation *affiliation = [self.affiliations objectAtIndex:i];
             if ([affiliation.ofAuthors containsObject:author]) {
-                [html appendFormat:@"%ld ", i+1];
+                [html appendFormat:@"%s%u", count != 0 ? ", " : "", i+1];
+                count++;
             }
         }
-        
+
         for (Correspondence *cor in self.correspondenceAt) {
             if (cor.ofAuthor == author)
                 [html appendString:@"*"];
         }
-        
+
         [html appendFormat:@"</sup><br/>"];
-        
+
     }
-    [html appendString:@"</h3></div>"];
-    
+    [html appendString:@"</h2></div><div id=\"affiliations\">"];
+    [html appendFormat:@"<ol>"];
+
     for (NSUInteger i = 0; i < self.affiliations.count; i++) {
         Affiliation *affiliation = [self.affiliations objectAtIndex:i];
         Organization *org = affiliation.toOrganization;
-        
-        if (org.department) {
-            if (org.section) {
-                [html appendFormat:@"<sup>%ld</sup> %@, %@, %@, %@<br/>", i+1, org.section, org.department, org.name, org.country];
-            } else {
-                [html appendFormat:@"<sup>%ld</sup> %@, %@, %@<br/>", i+1, org.department, org.name, org.country];
-            }
-        }
-        else {
-            [html appendFormat:@"<sup>%ld</sup> %@, %@<br/>", i+1, org.name, org.country];
-        }
+
+        [html appendFormat:@"<li>"];
+
+        if (org.section)
+            [html appendFormat:@"%@, ", org.department];
+
+        if (org.department)
+            [html appendFormat:@"%@, ", org.department];
+
+        [html appendFormat:@"%@, %@", org.name, org.country];
+        [html appendFormat:@"</li>"];
     }
-    
+
+    [html appendFormat:@"</ol>"];
     for (Correspondence *cor in self.correspondenceAt) {
-            [html appendFormat:@"<sup>*</sup> %@ ", cor.email];
+        [html appendFormat:@"</div><div id=\"correspondence\"><sup>*</sup> %@ ", cor.email];
     }
-    [html appendString:@"<br/>"];
-    
-    [html appendFormat:@"<p>%@</p>", [self.text formatHTML]];
-    
-    if (self.acknoledgements)
-        [html appendFormat:@"<p><h4>Acknowledgements</h4>%@</p>", [self.acknoledgements formatHTML]];
-    
-    if (self.references)
-        [html appendFormat:@"<p><h4>References</h4>%@</p>", [self.references formatHTML]];
-    
-    [html appendFormat:@"<div><small>DOI: %@</small></div>", self.doi];
-    [html appendFormat:@"<div><small>uuid: %@</small></div>", self.uuid];
+    [html appendString:@"</div><br/>"];
+
+    [html appendFormat:@"<div><p id=\"abstract\">%@</p></div>", [self.text formatHTML]];
+
+    if (self.acknoledgements && self.acknoledgements.length)
+        [html appendFormat:@"<div class=\"appendix\"><h4>Acknowledgements</h4><p>%@</p></div>", [self.acknoledgements formatHTML]];
+
+    if (self.references && self.references.length)
+        [html appendFormat:@"<div class=\"appendix\"><p><h4>References</h4><p>%@</p></div>", [self.references formatHTML]];
+
+    NSArray *legitFormats = @[ @"tiff", @"png", @"jpeg", @"pdf"];
+    if (self.nfigures) {
+        NSString *figID = [NSString stringWithFormat:@"%03d", self.figid];
+        NSLog(@"figure path: %@", figID);
+
+        __block NSString *fullpath = @"";
+
+        [legitFormats enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+            fullpath = [NSString stringWithFormat:@"%@.%@", figID, obj];
+            NSString *testPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:fullpath];
+            *stop = [[NSFileManager defaultManager] fileExistsAtPath:testPath];
+        }];
+
+        NSString *caption = self.caption;
+        if ([caption hasPrefix:@"Figure 1"]) {
+            caption = [caption substringFromIndex:8];
+        }
+
+        [html appendFormat:@"<div class=\"figure\"><img src=\"%@\" /><p>Figure 1:%@<p></div><br/>", fullpath, caption];
+    }
+
+    [html appendFormat:@"<div>doi: <a href=\"http://dx.doi.org/%@\">%@</a></div><br/>", self.doi, self.doi];
     [html appendString:@"</body></html>"];
+
     
     return html;
 }
